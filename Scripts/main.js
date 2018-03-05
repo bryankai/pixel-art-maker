739044d0436c49a4352f209b6d71ed24e80a8e03
Bryan Kai <brykai123@gmail.com>
2018-03-05 14:07:57 -0800

    Added click and drag painting ability.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,35 +1,34 @@
-// INITIALIZING
+////// CREATING GRID /////
 let cellGrid = document.querySelector('.cellGrid')
 let gridHeight = 15
 let gridWidth = 20
 createGrid()
-// Assigning variables
+
+////// VARIABLE DECLARATION  /////
 let cells = document.querySelectorAll('.cell')
 let colors = document.querySelectorAll('.color-palette div')
 let colorIndicator = document.getElementById('colorIndicator')
 let colorWheel = document.querySelector('input')
 let paintColor = 'white'
+let paintToggle = false
 colorIndicator.style.backgroundColor = paintColor
 
-// Adding Event Listeners
-for (let i = 0; i<cells.length; i++) {
-  cells[i].addEventListener('mousedown', render)
-  // Need to use mousedown, mouseneter, and mouseup
-  // If i click, change that cells
-  // If key is pressed, then do mouseenter
-  // also do mouseup?
-}
+///// EVENT LISTENERS /////
+// GRID
+cellGrid.addEventListener('click', togglePaint)
+cellGrid.addEventListener('mouseover', painting)
 
+// COLOR PALETTE
 for (let i = 0; i<colors.length; i++) {
   colors[i].addEventListener('click', chooseColor)
 }
-
 colorWheel.addEventListener('change', function (){
   paintColor=event.target.value
   colorIndicator.style.backgroundColor = paintColor
 })
 
-// Functions
+///// FUNCTIONS /////
+// GRID CREATION //
 function createGrid() {
   // Creates the grid based on the gridHeight and gridWidth dimensions stated above
   for (let i=0; i<gridHeight; i++) {
@@ -42,7 +41,6 @@ function createGrid() {
     }
   }
 }
-
 // Create a row
 function addRow() {
   const newRow = document.createElement("div");
@@ -50,18 +48,32 @@ function addRow() {
   return newRow
 }
 
-//Create a div
+// Create a div
 function addCell() {
   const newCell = document.createElement("div");
   newCell.classList.add('cell')
   return newCell
 }
 
-function render() {
-  event.target.style.backgroundColor = paintColor
+// PAINTING FUNCTIONS //
+function togglePaint(event) {
+  if (event.target.classList.contains('cell')) {
+    paintToggle = !paintToggle
+    event.target.style.backgroundColor = paintColor
+  }
 }
 
+function painting(event) {
+  if (event.target.classList.contains('cell')) {
+    if (paintToggle) {
+      event.target.style.backgroundColor = paintColor
+    }
+  }
+}
+
+// CHOOSE A COLOR //
 function chooseColor() {
+  paintToggle = false
   paintColor = event.target.style.backgroundColor
   colorIndicator.style.backgroundColor = paintColor
 }
